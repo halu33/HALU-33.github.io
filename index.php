fd3630f3d2f2afabeaf73f81a3633ca273fb5e33
HALU_33 <gamer.halu.33@gmail.com>
2024-01-20 06:24:22 +0900

    update index.php and style.css

--- index.php
+++ index.php
@@ -8,18 +8,17 @@ include "./src/php/counter.php";
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="HALU_33のプロフィール">
         <meta property="og:type" content="website">
-        <meta property="og:url" content="https://halu33.net/">
+        <meta property="og:url" content="https://halu33.net/MyProfile">
         <meta property="og:locale" content="ja_JP">
-        <link rel="stylesheet" href="./src/css/style.css"/>
+        <link rel="stylesheet" href="./src/css/style.css?v=1"/>
         <title>@HALU_33</title>
+        <meta name="description" content="HALU_33 Profile">
         <link rel="icon" href="./img/epril_icon.png">
         <link rel="apple-touch-icon" sizes="180x180" href="./img/epril_icon.png">
         <script src="https://kit.fontawesome.com/820bccf440.js" crossorigin="anonymous"></script>
     </head>
 
-
     <body>
-        <!-- モード切り替えボタン -->
         <div class="switch-button">
             <!-- ライトモード切り替えボタン -->
             <button id="lightModeButton" class="mode-button">
@@ -31,37 +30,36 @@ include "./src/php/counter.php";
             </button>
         </div>
 
-
         <!-- ヘッダー -->
         <div class="header">
-            <h1><strong><a href="https://halu33.net">@HALU_33</a></strong></h1>
+            <h1><strong><img src="./img/epril_icon.png" alt="icon" class="header-icon"><a href="https://halu33.net/MyProfile">@HALU_33</a></strong></h1>
         </div>
-
 
         <!-- SNS -->
         <div class="sns">
-            <a href="https://twitter.com/HALU_33" target="_blank">
-                <i class="fab fa-twitter"></i>
-                <div class="sns-name">@HALU_33</div>
-            </a>
-            <a href="https://discord.gg/UQMnbP85PB" target="_blank">
-                <i class="fab fa-discord"></i>
-                <div class="sns-name">個人鯖</div>
-            </a>
-            <a href="https://www.youtube.com/c/HALU_33" target="_blank">
-                <i class="fab fa-youtube"></i>
-                <div class="sns-name">@HALU_33</div>
-            </a>
-            <a href="https://open.spotify.com/user/i1l47xrnbi52fzlnbi8bw95f6?si=f22210590b4e477e&nd=1" target="_blank">
-                <i class="fab fa-spotify"></i>
-                <div class="sns-name">mna</div>
-            </a>
-            <a href="https://github.com/halu33" target="_blank">
-                <i class="fab fa-github"></i>
-                <div class="sns-name">halu33</div>
-            </a>
+            <table class="sns-table">
+                <tr>
+                    <td><a href="https://twitter.com/MK8DX_mkmg" target="_blank"><i class="fab fa-twitter"></i></a></td>
+                    <td><s>@HALU_33</s> → @MK8DX_mkmg ※アカウントが凍結されたためIDが変わりました</td>
+                </tr>
+                <tr>
+                    <td><a href="https://discord.gg/UQMnbP85PB" target="_blank"><i class="fab fa-discord"></i></a></td>
+                    <td>username: <code>halu_33</code>      ※個人鯖では通話の他に（遊びで）botの開発等行ってます</td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.youtube.com/c/HALU_33" target="_blank"><i class="fab fa-youtube"></i></a></td>
+                    <td>@HALU_33</td>
+                </tr>
+                <tr>
+                    <td><a href="https://open.spotify.com/user/i1l47xrnbi52fzlnbi8bw95f6?si=f22210590b4e477e&nd=1" target="_blank"><i class="fab fa-spotify"></i></a></td>
+                    <td>mna</a></td>
+                </tr>
+                <tr>
+                    <td><a href="https://github.com/halu33" target="_blank"><i class="fab fa-github"></i></a></td>
+                    <td>halu33</td>
+                </tr>
+            </table>
         </div>
-
 
         <!-- 詳細情報部分 -->
         <div class="info">
@@ -76,17 +74,21 @@ include "./src/php/counter.php";
                 </tr>
                 <tr>
                     <th>所在地</th>
-                    <td>日本 三重</td>
+                    <td>日本 三重県</td>
                 </tr>
                 <tr>
                     <th>学校</th>
-                    <td>どこかの高専 kosen20s</td>
+                    <td>どこかの高専 #kosen20s</td>
                 </tr>
                 <tr>
-                    <th>MK8DX(故)</th>
-                    <td>所属:Code RL ill Iruca その他化石チーム<br>
+                    <th>MK8DX</th>
+                    <td>所属:elf Code RL ill Iruca その他化石チーム<br>
                         150cc Lounge:<a href="https://www.mk8dx-lounge.com/PlayerDetails/10999" target="_blank">SenjuIroha</a>
                     </td>
+                </tr>
+                <tr>
+                    <th>ユメステ</th>
+                    <td>親指勢 多指置きプレイ🔰 23年7月～ レート950↑ 星章30%↑ <br> 推し: 新妻八恵、千寿いろは、白丸美兎、流石知冴</td>
                 </tr>
                 <tr>
                     <th>白猫プロジェクト(故)</th>
@@ -94,13 +96,8 @@ include "./src/php/counter.php";
                         推しキャラ: ヴィレータ、エプリル
                     </td>
                 </tr>
-                <tr>
-                    <th>ユメステ</th>
-                    <td>親指勢 23年7月～ レート950↑ 星章30%↑ <br> 推し: 新妻八恵、流石知冴、千寿いろは、白丸美兎</td>
-                </tr>
             </table>
         </div>
-
 
         <!-- NowPlaying -->
         <div id="now-playing-container">
@@ -112,12 +109,10 @@ include "./src/php/counter.php";
             </div>
         </div>
 
-
         <!-- アクセスカウンター -->
         <div class="access-counter">
             <?php echo str_pad($total_visitor_count, 8, '0', STR_PAD_LEFT); ?>
         </div>
-
 
         <script src="./src/js/ModeSwitch.js"></script>
         <script src="./src/js/NowPlaying.js"></script>
